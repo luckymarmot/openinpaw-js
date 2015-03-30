ea2d15e142d07f690ca73579fdedacd039b1ccd7
Micha Mazaheri <micha@mazaheri.me>
2015-03-30 21:54:50 +0200

    Improved paw.js

--- paw.js
+++ paw.js
@@ -1,5 +1,32 @@
-paw = {
-	'open':function(d){
-		alert('>> ' + d.deeplink);
+(function(window){
+	var _p = {};
+
+	/**
+	 * Check if user has the desktop Paw app installed, and calls `cb` when
+	 * completed.
+	 * @param cb A callback function.
+	 */
+	var _chkHasPaw = function(cb) {
+		var r = confirm('Do you have Paw?');
+		cb(r);
 	}
-}
+
+	/**
+	 * Opens in Paw with the given parameters object.
+	 * @param d The parameters.
+	 */
+	_p.open = function(d) {
+		_chkHasPaw(function(r) {
+			if (r) {
+				if ('undefined' !== typeof d.deeplink) {
+					document.location.href = d.deeplink;
+				}
+			}
+			else {
+				document.location.href = 'https://luckymarmot.com/paw';
+			}
+		});
+	}
+
+	window.paw = _p;
+})(window);
