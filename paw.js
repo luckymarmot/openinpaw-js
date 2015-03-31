be9e4a7aac734c47d7793a3642137279626f359e
Micha Mazaheri <micha@mazaheri.me>
2015-03-31 18:20:07 +0200

    Started Open in Paw styling.

--- paw.js
+++ paw.js
@@ -1,5 +1,22 @@
 (function(window){
 	/**
+	 * Web Fonts
+	 */
+
+	WebFontConfig = {
+		google: { families: [ 'Open+Sans:400,300:latin' ] }
+	};
+	(function() {
+		var wf = document.createElement('script');
+		wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+		'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+		wf.type = 'text/javascript';
+		wf.async = 'true';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(wf, s);
+	})();
+
+	/**
 	 * Cookies
 	 */
 	var cookies = {
