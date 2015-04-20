3a016f9ea58c99c3a0fb4e9446b76e16da617991
Micha Mazaheri <micha@mazaheri.me>
2015-04-20 19:16:16 +0200

    Copied static files.

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,8 @@ gulp.task('default', ['build']);
 
 gulp.task('build', [
   'compass',
-  'coffee'
+  'coffee',
+  'static'
 ]);
 
 gulp.task('clean', function (cb) {
@@ -20,7 +21,7 @@ gulp.task('clean', function (cb) {
   ], cb);
 });
 
-gulp.task('compass', function () {
+gulp.task('compass', ['static'], function () {
   return gulp.src('./sass/*.sass')
     .pipe(plumber())
     .pipe(compass({
@@ -33,7 +34,7 @@ gulp.task('compass', function () {
         style: 'compressed',
         css: './build/css/',
         sass: './sass/',
-        image: './images/'
+        image: './build/images/'
       }))
     .pipe(gulp.dest('./build/css/'));
 });
@@ -45,9 +46,15 @@ gulp.task('coffee', function() {
         bare:false
       }))
     .pipe(uglify({
-      mangle:false // disable mangle for now
+      mangle:true
     }))
     .pipe(gulp.dest('./build/js/'));
+});
+
+gulp.task('static', function() {
+  return gulp.src('./images/*')
+    .pipe(plumber())
+    .pipe(gulp.dest('./build/images/'));
 });
 
 gulp.task('watch', [], function() {
